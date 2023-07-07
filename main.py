0fb1b71763bdd7c2f11b4805f8a79e8276b2b67e
Mohammad <shmt.researcher@gmail.com>
2023-07-08 01:50:54 +0330

    3 Commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+
+def play_game():
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+
+    while True:
+        guess = input("Take a guess: ")
+        attempts += 1
+
+        try:
+            guess = int(guess)
+        except ValueError:
+            print("Invalid input! Please enter a number.")
+            continue
+
+        if guess < secret_number:
+            print("Too low! Try again.")
+        elif guess > secret_number:
+            print("Too high! Try again.")
+        else:
+            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            break
+
+    play_again = input("Do you want to play again? (yes/no): ")
+    if play_again.lower() == "yes":
+        play_game()
+    else:
+        print("Thank you for playing the game!")
+
+play_game()
